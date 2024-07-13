45b88de84f502ac9495094bd1e6a12aace02d185
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-07-14 03:48:07 +0900

    [BVVER24-149] feat: edit nickname 작업중

--- presentation/src/main/java/com/example/presentation/view/mypage/view/MypageEditProfileFragment.kt
+++ presentation/src/main/java/com/example/presentation/view/mypage/view/MypageEditProfileFragment.kt
@@ -1,17 +1,75 @@
 package com.example.presentation.view.mypage.view
 
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.example.presentation.R
 import com.example.presentation.base.BaseFragment
+import com.example.presentation.config.navigation.main.MainNavigationHandler
 import com.example.presentation.databinding.FragmentMypageEditProfileBinding
 import com.example.presentation.view.main.MainFragment
+import com.example.presentation.view.mypage.viewModel.MypageUserInfoViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MypageEditProfileFragment : BaseFragment<FragmentMypageEditProfileBinding>(R.layout.fragment_mypage_edit_profile) {
-    override fun init() {
+    @Inject
+    lateinit var mainNavigationHandler: MainNavigationHandler
+    private val mypageUserInfoViewModel: MypageUserInfoViewModel by viewModels()
 
+    private lateinit var emptyErrorText: String
+    private lateinit var invalidErrorText: String
+
+    override fun init() {
+        backButton()
+        setErrorText()
+        setInputHelper()
+        setObserve()
+    }
+    private fun setInputHelper() {
+        binding.tilOnboardingEditNick.error = emptyErrorText
+        mypageUserInfoViewModel.setValidNickName(false)
+
+        mypageUserInfoViewModel.nickName.observe(this) { nickName ->
+            binding.tilOnboardingEditNick.error = if (nickName.isNullOrEmpty()) {
+                // 비어 있는 경우
+                mypageUserInfoViewModel.setValidNickName(false)
+                emptyErrorText
+            } else {
+                if (mypageUserInfoViewModel.validateNickName(nickName)) {
+                    // 유효한 경우
+                    mypageUserInfoViewModel.setValidNickName(true)
+                    ""
+                } else {
+                    // 유효하지 않은 경우
+                    mypageUserInfoViewModel.setValidNickName(false)
+                    invalidErrorText
+                }
+            }
+        }
+    }
+    private fun setErrorText() {
+        emptyErrorText = getString(R.string.helper_text_nickname_default)
+        invalidErrorText = getString(R.string.helper_text_nickname_check)
+    }
+    private fun setObserve() {
+        mypageUserInfoViewModel.validNickName.observe(this) {
+            binding.btnOnboardingNext.isEnabled = mypageUserInfoViewModel.checkValid()
+            logMessage("${mypageUserInfoViewModel.nickName}")
+        }
+        mypageUserInfoViewModel.userInfoState.observe(this) { isUserInfoState ->
+            if (isUserInfoState) {
+//                navigateToMain()
+            }else{
+                showToast("유저 추가 정보 입력 오류")
+            }
+        }
+    }
+
+
+    private fun backButton(){
         binding.ibBackUp.setOnClickListener {
-            requireActivity().supportFragmentManager.popBackStack()
+            mainNavigationHandler.popBackStack()
         }
     }
 }
--- presentation/src/main/java/com/example/presentation/view/mypage/viewModel/MypageUserInfoViewModel.kt
+++ presentation/src/main/java/com/example/presentation/view/mypage/viewModel/MypageUserInfoViewModel.kt
@@ -21,6 +21,17 @@ class MypageUserInfoViewModel @Inject constructor(
     private val _userInfo = MutableLiveData<MypageUserInfo>()
     val userInfo:LiveData<MypageUserInfo> = _userInfo
 
+    val nickName = MutableLiveData<String?>()
+    private var _validNickName = MutableLiveData<Boolean>()
+    private var _validVeganLevel = MutableLiveData<Boolean>()
+
+    // 이름, 비건 레벨 유효성 검사 LiveData
+    val validNickName: LiveData<Boolean> = _validNickName
+    val validVeganLevel: LiveData<Boolean> = _validVeganLevel
+
+    private val _userInfoState = MutableLiveData<Boolean>(false)
+    val userInfoState: LiveData<Boolean> get() = _userInfoState
+
     init {
         getUserInfo()
     }
@@ -41,5 +52,32 @@ class MypageUserInfoViewModel @Inject constructor(
                 Timber.e("patchUserVeganType onFailure")
             }
         }
+    }
+
+    //edit profile
+    fun setValidNickName(check: Boolean) {
+        _validNickName.value = check
+    }
+    fun validateNickName(input: String): Boolean {
+        // 입력이 2~12자인지 확인
+        if (input.length !in 2..12) {
+            return false
+        }
+
+        // 입력이 한글 또는 영문으로 이루어져 있는지 확인
+        if (!input.all { it in '가'..'힣' || it in 'a'..'z' || it in 'A'..'Z' }) {
+            return false
+        }
+
+        // 입력에 공백이 포함되어 있는지 확인
+        if (input.contains(" ")) {
+            return false
+        }
+
+        // 모든 조건을 만족하면 true 반환
+        return true
+    }
+    fun checkValid(): Boolean {
+        return validNickName.value ?: false && validVeganLevel.value ?: false
     }
 }
